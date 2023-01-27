9717cfdcd93a56c86afcaf0e73bff7bf529b4c4d
Kwesi Batchelor <kwesibatchelor@Kwesis-iMac.home>
2023-01-26 23:36:03 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import random
+
+def roll_dice():
+    roll = input("Roll the dice? (Yes/No): ")
+    while roll.lower() == "Yes".lower():
+        dice1 = random.randint(1, 6)
+        dice2 = random.randint(1, 6)
+
+        print("dice rolled: {} and {}".format(dice1, dice2))
+
+        roll = input("Roll again? (Yes/No): ")
+    
+roll_dice()
